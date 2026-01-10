7c2b2d41b5acb39d301f33ee162ec315d5b38516
Pao2PAO <clavel.paolo.esporas@gmail.com>
2026-01-11 02:23:40 +0800

    Update Device.java

--- SmartHomeManagementSystem/Device.java
+++ SmartHomeManagementSystem/Device.java
@@ -1,35 +1,33 @@
 public class Device {
-    // 1.a Attributes
+   
     protected String deviceName;
-    protected String status; // only accepts ON or OFF
+    protected String status; 
     protected String location;
 
-    // 3. Static variable to count devices
+    
     static int deviceCount = 0;
 
-    // 1.b Constructors
-    // Default constructor
+   
     public Device() {
         this.deviceName = "Unknown";
         this.status = "OFF";
         this.location = "Unknown";
-        deviceCount++; // Increment count
-    }
+        deviceCount++; 
 
-    // Parameterized constructor
+    
     public Device(String deviceName, String status, String location) {
         this.deviceName = deviceName;
-        // Simple validation for status
+        
         if (status.equals("ON") || status.equals("OFF")) {
             this.status = status;
         } else {
-            this.status = "OFF"; // Default to OFF if invalid
+            this.status = "OFF"; 
         }
         this.location = location;
-        deviceCount++; // Increment count
+        deviceCount++; 
     }
 
-    // 1.c Methods
+    
     public void turnOn() {
         this.status = "ON";
         System.out.println(deviceName + " is now ON.");
@@ -46,8 +44,9 @@ public class Device {
         System.out.println("Location: " + location);
     }
 
-    // 3. Static method to display total devices
+    
     public static void showDeviceCount() {
         System.out.println("Total Devices Created: " + deviceCount);
     }
+
 }
