d5efc67d50fc38b4c34eeaaaa072fe7061e44a68
Pao2PAO <clavel.paolo.esporas@gmail.com>
2026-01-11 02:24:43 +0800

    Update SmartHomeController.java

--- SmartHomeManagementSystem/SmartHomeController.java
+++ SmartHomeManagementSystem/SmartHomeController.java
@@ -1,12 +1,12 @@
 public class SmartHomeController {
     private Device[] deviceList;
 
-    // 7.a Accepts device array as parameter
+   
     public SmartHomeController(Device[] devices) {
         this.deviceList = devices;
     }
 
-    // 7.b Method to turn ALL devices ON
+    
     public void turnAllOn() {
         System.out.println("\n--- Turning ALL Devices ON ---");
         for (Device d : deviceList) {
@@ -14,11 +14,12 @@ public class SmartHomeController {
         }
     }
 
-    // 7.b Method to turn ALL devices OFF
+   
     public void turnAllOff() {
         System.out.println("\n--- Turning ALL Devices OFF ---");
         for (Device d : deviceList) {
             d.turnOff();
         }
     }
+
 }
