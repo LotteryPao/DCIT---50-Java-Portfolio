d53ae58c89aa832cc0d0c329aa0e358d454800ef
Pao2PAO <clavel.paolo.esporas@gmail.com>
2026-01-11 02:49:01 +0800

    Delete SmartHomeControlSystem/MyOrderSystem/src/com/store directory

--- SmartHomeControlSystem/MyOrderSystem/src/com/store/order/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.store.order;
-
-// Task 2: Define an Enum for Order Status
-public enum OrderStatus {
-    PENDING,
-    PAID,
-    SHIPPED,
-    CANCELLED
-}
--- SmartHomeControlSystem/MyOrderSystem/src/com/store/payment/Payable.java
+++
@@ -1,7 +0,0 @@
-package com.store.payment;
-
-// Task 4: Create a Payment Interface
-public interface Payable {
-    void pay();
-}
-
