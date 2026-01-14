9ab1c213554271fbcfbaf71dd09a4263d55d8e8e
Paolo Clavel <clavel.paolo.esporas@gmail.com>
2026-01-14 13:50:44 +0800

    Update TaskManager.java

--- ToDoListProject/src/controller/TaskManager.java
+++ ToDoListProject/src/controller/TaskManager.java
@@ -4,27 +4,28 @@ import model.Task;
 import java.util.ArrayList;
 
 public class TaskManager {
-    // List to hold all tasks
+    // The list to hold all the tasks
     private ArrayList<Task> taskList;
 
     public TaskManager() {
         this.taskList = new ArrayList<>();
     }
 
-    // Add a task to the list
+    // Adding a task to the list 
     public void addTask(Task task) {
         taskList.add(task);
     }
 
-    // Get the whole list (needed for the table)
+    // Getting the whole list that is needed for the table 
     public ArrayList<Task> getAllTasks() {
         return taskList;
     }
 
-    // Helper to generate a simple ID based on list size
+    // Used for generating a simple ID based on list size
     public String generateNextId() {
         int nextId = taskList.size() + 1;
         return "TASK-" + nextId;
     }
 }
+
 
