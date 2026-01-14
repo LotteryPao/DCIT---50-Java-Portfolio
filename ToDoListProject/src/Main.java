64d5a783526cb78cc4f30d7eccbc460c980a5693
Paolo Clavel <clavel.paolo.esporas@gmail.com>
2026-01-14 13:55:15 +0800

    Update Main.java

--- ToDoListProject/src/Main.java
+++ ToDoListProject/src/Main.java
@@ -5,17 +5,18 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Run UI in the Event Dispatch Thread (good practice for Swing)
+        
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // a. Instantiate TaskManager
+                
                 TaskManager manager = new TaskManager();
 
-                // b. Pass instance into UI
+                
                 MainFrame frame = new MainFrame(manager);
                 frame.setVisible(true);
             }
         });
     }
+
 }
